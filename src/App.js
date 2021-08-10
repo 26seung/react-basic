8bafff1ac64b16b849a0180b5e39e5f2f1aa2c42
26seung <yousi7@naver.com>
2021-08-10 22:33:47 +0900

    context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import Main from './components/Main';
+import About from './contextAPI/pages/About';
+import Profile from './contextAPI/pages/Profile';
+import UserStore from './contextAPI/store/users';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>      
+      <Switch>
+        <Route exact path={"/"} component={Main} />
+        <UserStore>
+          <Route exact path={"/about"} component={About} />
+          <Route exact path={"/profile"} component={Profile} />
+        </UserStore>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
